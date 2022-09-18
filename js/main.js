82295f3f675d5735c67df66b40184e6e15d3528a
RomaDel1fe <romadelife@gmail.com>
2022-09-19 01:54:45 +0300

    added basic version about course

--- js/main.js
+++ js/main.js
@@ -10,3 +10,13 @@ $(document).ready(function(){
       $('.content').toggleClass('blur')
    })
 })
+
+$(document).ready(function(){
+   $('.accordion__title').click(function(event){
+      if($('.accordion').hasClass('one')){
+         $('.accordion__title').not($(this)).removeClass('active');
+         $('.accordion__content').not($(this).next()).slideUp(300);
+      }
+      $(this).toggleClass('active').next().slideToggle(300);
+   });
+});
